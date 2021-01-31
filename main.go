835f55959222cb435478f90d271d1ecd7b7d2a59
Shashank Pachava <spachava753@gmail.com>
2021-01-31 17:25:22 -0500

    Refactor main.go

--- main.go
+++ main.go
@@ -5,34 +5,48 @@ import (
 	"github.com/mitchellh/go-homedir"
 	"github.com/spachava753/kpkg/cmd"
 	"github.com/spachava753/kpkg/pkg/config"
+	"github.com/spachava753/kpkg/pkg/get/linkerd2"
 	"os"
+	"runtime"
 )
 
 func main() {
-	hDir, err := homedir.Dir()
-	if err != nil {
+	if err := run(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
-		return
 	}
-	config.CreateBinPath(hDir)
+}
+
+func run() error {
+	hDir, err := homedir.Dir()
+	if err != nil {
+		return err
+	}
+
+	root, err := config.CreatePath(hDir)
+	if err != nil {
+		return err
+	}
 
 	rootCmd := cmd.MakeRoot()
 	getCmd := cmd.MakeGet()
 	listCmd := cmd.MakeList()
 
+	ld2 := linkerd2.MakeTool(root, runtime.GOOS, runtime.GOARCH)
+
 	getCmd.AddCommand(
-		cmd.MakeGetLinkerd2(),
+		cmd.MakeGetLinkerd2(ld2),
 	)
 
 	listCmd.AddCommand(
-		cmd.MakeListLinkerd2(),
+		cmd.MakeListLinkerd2(ld2),
 	)
 
 	rootCmd.AddCommand(getCmd, listCmd)
 
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		return err
 	}
+
+	return nil
 }
