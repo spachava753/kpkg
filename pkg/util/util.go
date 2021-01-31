0dae1432277a2a7dcafa574cd2ab805a271dcd8f
Shashank Pachava <spachava753@gmail.com>
2021-01-31 17:23:21 -0500

    Add util

---
+++ pkg/util/util.go
@@ -0,0 +1,11 @@
+package util
+
+// ContainsString returns true if a string is present in a slice.
+func ContainsString(s []string, v string) bool {
+	for _, vv := range s {
+		if vv == v {
+			return true
+		}
+	}
+	return false
+}
