4d88b756c74abf01e8745f01e7a6f56bbcecc78f
Shashank Pachava <spachava753@gmail.com>
2021-01-31 17:25:03 -0500

    Modify get command to use args instead of flag
    Modify get command to use args instead of flag to specify version

--- cmd/get.go
+++ cmd/get.go
@@ -2,15 +2,21 @@ package cmd
 
 import "github.com/spf13/cobra"
 
-const CliVersionFlag = "version"
+const CliForceInstallFlag = "force"
 
 func MakeGet() *cobra.Command {
 	var getCmd = &cobra.Command{
 		Use:   "get",
 		Short: "Get or install a binary",
 		Long:  `Get or install a binary. By default, the latest version of the binary will be downloaded`,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if err := cobra.MaximumNArgs(1)(cmd, args); err != nil {
+				return err
+			}
+			return nil
+		},
 	}
 
-	getCmd.PersistentFlags().String(CliVersionFlag, "latest", "specify a version when downloading. Default value is latest")
+	getCmd.PersistentFlags().Bool(CliForceInstallFlag, false, "force a re-install if already installed")
 	return getCmd
 }
