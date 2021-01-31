10722251bd093b38719bda1290e7c93ed999aff1
Shashank Pachava <spachava753@gmail.com>
2021-01-30 21:54:41 -0500

    Convert version flag name in get command to constant

--- cmd/get.go
+++ cmd/get.go
@@ -2,11 +2,15 @@ package cmd
 
 import "github.com/spf13/cobra"
 
+const CliVersionFlag = "version"
+
 func MakeGet() *cobra.Command {
 	var getCmd = &cobra.Command{
 		Use:   "get",
 		Short: "Get or install a binary",
 		Long:  `Get or install a binary. By default, the latest version of the binary will be downloaded`,
 	}
+
+	getCmd.Flags().String(CliVersionFlag, "latest", "specify a version when downloading. Default value is latest")
 	return getCmd
 }
