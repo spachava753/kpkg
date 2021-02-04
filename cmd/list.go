03365178794941012544b119cff8b8745cbd67da
Shashank Pachava <spachava753@gmail.com>
2021-02-03 22:37:35 -0500

    Convert "installed" flag to POSIX variant for list command. Add Args function

--- cmd/list.go
+++ cmd/list.go
@@ -3,14 +3,26 @@ package cmd
 import "github.com/spf13/cobra"
 
 const CliInstalledVersionsFlag = "installed"
+const CliInstalledVersionsShorthandFlag = "i"
 
 func MakeList() *cobra.Command {
 	var listCmd = &cobra.Command{
 		Use:   "list",
 		Short: "List versions of a specific binary",
 		Long:  `List different version candidates for installation of a specific binary`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if !cmd.Flags().Changed(CliInstalledVersionsFlag) {
+				return cobra.ExactArgs(1)(cmd, args)
+			}
+			return nil
+		},
 	}
 
-	listCmd.PersistentFlags().Bool(CliInstalledVersionsFlag, false, "show only installed versions")
+	listCmd.PersistentFlags().BoolP(
+		CliInstalledVersionsFlag,
+		CliInstalledVersionsShorthandFlag,
+		false,
+		"show only installed versions",
+	)
 	return listCmd
 }
