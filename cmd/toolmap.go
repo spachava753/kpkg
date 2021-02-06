8c2b23d55554bf0d00824322f7a80ccf3b7b3d08
Shashank Pachava <spachava753@gmail.com>
2021-02-06 00:53:08 -0500

    Add kind

--- cmd/toolmap.go
+++ cmd/toolmap.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"github.com/spachava753/kpkg/pkg/tool"
+	"github.com/spachava753/kpkg/pkg/tool/kind"
 	"github.com/spachava753/kpkg/pkg/tool/kubectl"
 	"github.com/spachava753/kpkg/pkg/tool/linkerd2"
 )
@@ -11,5 +12,6 @@ func GetTools(basePath string, os, arch string) []tool.Binary {
 	return []tool.Binary{
 		linkerd2.MakeBinary(basePath, os, arch),
 		kubectl.MakeBinary(basePath, os, arch),
+		kind.MakeBinary(basePath, os, arch),
 	}
 }
