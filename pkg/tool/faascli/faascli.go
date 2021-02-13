6bec61eb5a5723d1486c76a42524b4fb4d6cab9e
Shashank Pachava <spachava753@gmail.com>
2021-02-13 14:20:06 -0500

    Refactor faascli.go

--- pkg/tool/faascli/faascli.go
+++ pkg/tool/faascli/faascli.go
@@ -1,20 +1,16 @@
 package faascli
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type faasCliTool struct {
 	arch,
 	os string
+	tool.GithubReleaseTool
 }
 
 func (l faasCliTool) Extract(artifactPath, _ string) (string, error) {
@@ -40,7 +36,7 @@ func (l faasCliTool) MakeUrl(version string) (string, error) {
 	}
 	version = v.String()
 
-	url := fmt.Sprintf("https://github.com/openfaas/faas-cli/releases/download/%s/faas-cli", version)
+	url := fmt.Sprintf("%s%s/faas-cli", l.MakeReleaseUrl(), version)
 	switch {
 	case l.os == "darwin" && l.arch == "amd64":
 		url += "-darwin"
@@ -59,52 +55,10 @@ func (l faasCliTool) MakeUrl(version string) (string, error) {
 	return url, nil
 }
 
-func (l faasCliTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "openfaas", "faas-cli", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage && len(releases) < 20 {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "openfaas", "faas-cli", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 20 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return faasCliTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("openfaas", "faas-cli", 20),
 	}
 }
