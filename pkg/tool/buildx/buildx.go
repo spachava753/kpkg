f5d34daca23294b59f3aa16a3403dab78e72f54f
Shashank Pachava <spachava753@gmail.com>
2021-02-13 14:26:16 -0500

    Refactor buildx.go

--- pkg/tool/buildx/buildx.go
+++ pkg/tool/buildx/buildx.go
@@ -1,24 +1,16 @@
 package buildx
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type buildxTool struct {
 	arch,
 	os string
-}
-
-func (l buildxTool) Extract(artifactPath, _ string) (string, error) {
-	return artifactPath, nil
+	tool.GithubReleaseTool
 }
 
 func (l buildxTool) Name() string {
@@ -66,59 +58,17 @@ func (l buildxTool) MakeUrl(version string) (string, error) {
 	default:
 		return "", &kpkgerr.UnsupportedRuntimeErr{Binary: l.Name()}
 	}
-	url := fmt.Sprintf("https://github.com/docker/buildx/releases/download/v%s/buildx-v%s.%s-%s", version, version, l.os, l.arch)
+	url := fmt.Sprintf("%sv%s/buildx-v%s.%s-%s", l.MakeReleaseUrl(), version, version, l.os, l.arch)
 	if l.os == "windows" {
 		url += ".exe"
 	}
 	return url, nil
 }
 
-func (l buildxTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "docker", "buildx", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "docker", "buildx", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 15 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return buildxTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("docker", "buildx", 20),
 	}
 }
