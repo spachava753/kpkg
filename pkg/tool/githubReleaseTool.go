bffd44d19fa7f0a9dca4e35a8ebde04403c5ab0a
Shashank Pachava <spachava753@gmail.com>
2021-02-13 13:45:56 -0500

    Refactor opa.go

--- pkg/tool/githubReleaseTool.go
+++ pkg/tool/githubReleaseTool.go
@@ -11,8 +11,12 @@ import (
 )
 
 type GithubReleaseTool struct {
-	org, repo string
-	max       uint
+	owner, repo string
+	max         uint
+}
+
+func (l GithubReleaseTool) MakeReleaseUrl() string {
+	return fmt.Sprintf("https://github.com/%s/%s/releases/download/", l.owner, l.repo)
 }
 
 func (l GithubReleaseTool) Extract(artifactPath, _ string) (string, error) {
@@ -22,13 +26,13 @@ func (l GithubReleaseTool) Extract(artifactPath, _ string) (string, error) {
 func (l GithubReleaseTool) Versions() ([]string, error) {
 	client := github.NewClient(nil)
 	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), l.org, l.repo, nil)
+	releases, resp, err := client.Repositories.ListReleases(context.Background(), l.owner, l.repo, nil)
 	if err != nil {
 		return nil, err
 	}
 	var r []*github.RepositoryRelease
 	for resp != nil && resp.NextPage != resp.LastPage && uint(len(releases)) < l.max {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), l.org, l.repo, &github.ListOptions{
+		r, resp, err = client.Repositories.ListReleases(context.Background(), l.owner, l.repo, &github.ListOptions{
 			Page:    resp.NextPage,
 			PerPage: int(l.max) - len(releases),
 		})
--- pkg/tool/opa/opa.go
+++ pkg/tool/opa/opa.go
@@ -1,24 +1,16 @@
 package opa
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type opaTool struct {
 	arch,
 	os string
-}
-
-func (l opaTool) Extract(artifactPath, _ string) (string, error) {
-	return artifactPath, nil
+	tool.GithubReleaseTool
 }
 
 func (l opaTool) Name() string {
@@ -54,64 +46,17 @@ func (l opaTool) MakeUrl(version string) (string, error) {
 	default:
 		return "", &kpkgerr.UnsupportedRuntimeErr{Binary: l.Name()}
 	}
-	url := fmt.Sprintf("https://github.com/open-policy-agent/opa/releases/download/v%s/opa_%s_amd64", version, l.os)
+	url := fmt.Sprintf("%sv%s/opa_%s_amd64", l.GithubReleaseTool.MakeReleaseUrl(), version, l.os)
 	if l.os == "windows" {
 		url = url + ".exe"
 	}
 	return url, nil
 }
 
-func (l opaTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "open-policy-agent", "opa", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "open-policy-agent", "opa", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 15 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	// dont need too many releases
-	if len(vs) > 15 {
-		vs = vs[:15]
-	}
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return opaTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("open-policy-agent", "opa", 15),
 	}
 }
