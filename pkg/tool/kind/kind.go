91e81562d81619067808126d490edaf1b66ae4bc
Shashank Pachava <spachava753@gmail.com>
2021-02-13 13:53:44 -0500

    Refactor kind.go

--- pkg/tool/kind/kind.go
+++ pkg/tool/kind/kind.go
@@ -1,20 +1,16 @@
 package kind
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type kindTool struct {
 	arch,
 	os string
+	tool.GithubReleaseTool
 }
 
 func (l kindTool) Extract(artifactPath, _ string) (string, error) {
@@ -55,57 +51,14 @@ func (l kindTool) MakeUrl(version string) (string, error) {
 	default:
 		return "", &kpkgerr.UnsupportedRuntimeErr{Binary: l.Name()}
 	}
-	url := fmt.Sprintf("https://github.com/kubernetes-sigs/kind/releases/download/v%s/kind-%s-%s", version, l.os, l.arch)
+	url := fmt.Sprintf("%sv%s/kind-%s-%s", l.MakeReleaseUrl(), version, l.os, l.arch)
 	return url, nil
-}
-
-func (l kindTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "kubernetes-sigs", "kind", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "kubernetes-sigs", "kind", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 100,
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	// sort results
-	return versions, nil
 }
 
 func MakeBinary(os, arch string) tool.Binary {
 	return kindTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("kubernetes-sigs", "kind", 20),
 	}
 }
