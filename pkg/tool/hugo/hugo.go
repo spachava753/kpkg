adf1519f0522c5c68393072e0edd73b38ee3a9a7
Shashank Pachava <spachava753@gmail.com>
2021-02-13 14:11:57 -0500

    Refactor hugo.go

--- pkg/tool/hugo/hugo.go
+++ pkg/tool/hugo/hugo.go
@@ -1,22 +1,18 @@
 package hugo
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
 	"os"
 	"path/filepath"
-	"sort"
-	"strings"
 )
 
 type hugoTool struct {
 	arch,
 	os string
+	tool.GithubReleaseTool
 }
 
 func (l hugoTool) Extract(artifactPath, _ string) (string, error) {
@@ -42,7 +38,9 @@ func (l hugoTool) ShortDesc() string {
 }
 
 func (l hugoTool) LongDesc() string {
-	return `Hugo is a static HTML and CSS website generator written in Go. It is optimized for speed, ease of use, and configurability. Hugo takes a directory with content and templates and renders them into a full HTML website`
+	return `Hugo is a static HTML and CSS website generator written in Go. 
+It is optimized for speed, ease of use, and configurability. 
+Hugo takes a directory with content and templates and renders them into a full HTML website`
 }
 
 func (l hugoTool) MakeUrl(version string) (string, error) {
@@ -114,52 +112,10 @@ func (l hugoTool) MakeUrl(version string) (string, error) {
 	return url + ".tar.gz", nil
 }
 
-func (l hugoTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "gohugoio", "hugo", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage && len(releases) < 20 {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "gohugoio", "hugo", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 20 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return hugoTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("gohugoio", "hugo", 20),
 	}
 }
