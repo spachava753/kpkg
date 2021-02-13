77575deb75ebe484cc471b0f42d51eb8e472e19b
Shashank Pachava <spachava753@gmail.com>
2021-02-13 13:52:25 -0500

    Refactor kops.go

--- pkg/tool/kops/kops.go
+++ pkg/tool/kops/kops.go
@@ -1,20 +1,16 @@
 package kops
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type kopsTool struct {
 	arch,
 	os string
+	tool.GithubReleaseTool
 }
 
 func (l kopsTool) Extract(artifactPath, _ string) (string, error) {
@@ -41,7 +37,7 @@ func (l kopsTool) MakeUrl(version string) (string, error) {
 	}
 	version = v.String()
 
-	url := fmt.Sprintf("https://github.com/kubernetes/kops/releases/download/v%s/kops-%s-%s", version, l.os, l.arch)
+	url := fmt.Sprintf("%sv%s/kops-%s-%s", l.MakeReleaseUrl(), version, l.os, l.arch)
 	switch {
 	case l.os == "darwin" && l.arch == "amd64",
 		l.os == "windows" && l.arch == "amd64",
@@ -54,53 +50,10 @@ func (l kopsTool) MakeUrl(version string) (string, error) {
 	return url, nil
 }
 
-func (l kopsTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "kubernetes", "kops", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage && len(releases) < 20 {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "kubernetes", "kops", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 20 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	// sort results
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return kopsTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("kubernetes", "kops", 20),
 	}
 }
