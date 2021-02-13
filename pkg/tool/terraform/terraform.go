ced1a505c7449864455fb21b436f095c0e79427f
Shashank Pachava <spachava753@gmail.com>
2021-02-13 13:42:29 -0500

    Refactor terraform.go

--- pkg/tool/terraform/terraform.go
+++ pkg/tool/terraform/terraform.go
@@ -1,24 +1,16 @@
 package terraform
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type terraformTool struct {
 	arch,
 	os string
-}
-
-func (l terraformTool) Extract(artifactPath, _ string) (string, error) {
-	return artifactPath, nil
+	tool.GithubReleaseTool
 }
 
 func (l terraformTool) Name() string {
@@ -76,57 +68,10 @@ func (l terraformTool) MakeUrl(version string) (string, error) {
 	return url, nil
 }
 
-func (l terraformTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "hashicorp", "terraform", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "hashicorp", "terraform", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 15 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	// dont need too many releases
-	if len(vs) > 20 {
-		vs = vs[:20]
-	}
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return terraformTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("hashicorp", "terraform", 20),
 	}
 }
