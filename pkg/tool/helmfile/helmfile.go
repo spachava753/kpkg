93e7e03934a48a8e14ee1e77cc0db7450fdae10d
Shashank Pachava <spachava753@gmail.com>
2021-02-13 14:13:03 -0500

    Refactor helmfile.go

--- pkg/tool/helmfile/helmfile.go
+++ pkg/tool/helmfile/helmfile.go
@@ -1,20 +1,16 @@
 package helmfile
 
 import (
-	"context"
 	"fmt"
 	"github.com/Masterminds/semver"
-	"github.com/google/go-github/v33/github"
 	kpkgerr "github.com/spachava753/kpkg/pkg/error"
 	"github.com/spachava753/kpkg/pkg/tool"
-	"github.com/thoas/go-funk"
-	"sort"
-	"strings"
 )
 
 type helmFileTool struct {
 	arch,
 	os string
+	tool.GithubReleaseTool
 }
 
 func (l helmFileTool) Extract(artifactPath, version string) (string, error) {
@@ -52,59 +48,17 @@ func (l helmFileTool) MakeUrl(version string) (string, error) {
 	default:
 		return "", &kpkgerr.UnsupportedRuntimeErr{Binary: l.Name()}
 	}
-	url := fmt.Sprintf("https://github.com/roboll/helmfile/releases/download/v%s/helmfile_%s_%s", version, l.os, l.arch)
+	url := fmt.Sprintf("%sv%s/helmfile_%s_%s", l.MakeReleaseUrl(), version, l.os, l.arch)
 	if l.os == "windows" {
 		return url + ".exe", nil
 	}
 	return url, nil
 }
 
-func (l helmFileTool) Versions() ([]string, error) {
-	client := github.NewClient(nil)
-	var resp *github.Response
-	releases, resp, err := client.Repositories.ListReleases(context.Background(), "roboll", "helmfile", nil)
-	if err != nil {
-		return nil, err
-	}
-	var r []*github.RepositoryRelease
-	for resp != nil && resp.NextPage != resp.LastPage && len(releases) < 20 {
-		r, resp, err = client.Repositories.ListReleases(context.Background(), "roboll", "helmfile", &github.ListOptions{
-			Page:    resp.NextPage,
-			PerPage: 20 - len(releases),
-		})
-		if err != nil {
-			return nil, err
-		}
-		releases = append(releases, r...)
-	}
-
-	releases = funk.Filter(releases, func(release *github.RepositoryRelease) bool {
-		return !release.GetPrerelease() && !strings.Contains(release.GetTagName(), "rc")
-	}).([]*github.RepositoryRelease)
-
-	vs := make([]*semver.Version, len(releases))
-	for i, release := range releases {
-		v, err := semver.NewVersion(release.GetTagName())
-		if err != nil {
-			return nil, fmt.Errorf("error parsing version: %w", err)
-		}
-
-		vs[i] = v
-	}
-
-	sort.Sort(sort.Reverse(semver.Collection(vs)))
-
-	versions := make([]string, 0, len(vs))
-	for _, v := range vs {
-		versions = append(versions, v.String())
-	}
-
-	return versions, nil
-}
-
 func MakeBinary(os, arch string) tool.Binary {
 	return helmFileTool{
-		arch: arch,
-		os:   os,
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("roboll", "helmfile", 20),
 	}
 }
