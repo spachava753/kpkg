5b16869bd8b943efbc6a578a25d48bf06001cc1f
Shashank Pachava <spachava753@gmail.com>
2021-02-18 22:11:36 -0500

    Fix build script

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -v `pwd`:/usr/src/myapp -v `pwd`/vendor:/go/src -w /usr/src/myapp golang:1.14 /usr/src/myapp/build_app.sh
+#docker run --rm -v `pwd`:/usr/src/myapp -v `pwd`/vendor:/go/src -w /usr/src/myapp golang:1.14 /usr/src/myapp/build_app.sh
+docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp golang:1.14 /usr/src/myapp/build_app.sh
