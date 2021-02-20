32269286cb5d00f047f0651e8330850d70a264f6
Shashank Pachava <spachava753@gmail.com>
2021-02-19 22:04:29 -0500

    Modify build.yml. Remove build scripts

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#docker run --rm -v `pwd`:/usr/src/myapp -v `pwd`/vendor:/go/src -w /usr/src/myapp golang:1.14 /usr/src/myapp/build_app.sh
-docker run --rm -v $(pwd):/usr/src/myapp -w /usr/src/myapp golang:1.14 /usr/src/myapp/build_app.sh
--- build_app.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# build all three versions windows, linux, mac, in parallel
-make build -j3
