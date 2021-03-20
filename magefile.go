9d95512f78f451c3d2f362a4491a40f78e4b0c78
Shashank Pachava <spachava753@gmail.com>
2021-03-20 01:43:44 -0400

    Add target to generate code for new github release tool

--- magefile.go
+++ magefile.go
@@ -4,7 +4,10 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"strings"
+	"text/template"
 
 	"github.com/magefile/mage/mg" // mg contains helpful utility functions, like Deps
 )
@@ -45,4 +48,85 @@ func Clean() error {
 	fmt.Println("Cleaning...")
 	cmd := exec.Command("rm", "-rf", "./kpkg")
 	return cmd.Run()
+}
+
+func GenGithubTool(toolName, org, repoName string) error {
+	templateCode := `package {{ .pkgName }}
+
+import (
+	"fmt"
+	"github.com/Masterminds/semver"
+	kpkgerr "github.com/spachava753/kpkg/pkg/error"
+	"github.com/spachava753/kpkg/pkg/tool"
+)
+
+type {{ .structName }} struct {
+	arch,
+	os string
+	tool.GithubReleaseTool
+}
+
+func (l {{ .structName }}) Name() string {
+	return "{{ .toolName }}"
+}
+
+func (l {{ .structName }}) ShortDesc() string {
+	return "REPLACE ME"
+}
+
+func (l {{ .structName }}) LongDesc() string {
+	return "REPLACE ME"
+}
+
+func (l {{ .structName }}) MakeUrl(version string) (string, error) {
+	v, err := semver.NewVersion(version)
+	if err != nil {
+		return "", err
+	}
+	version = v.String()
+
+	url := fmt.Sprintf("%s%s/{{ .repoName }}", l.MakeReleaseUrl(), version)
+	switch {
+	case l.os == "darwin" && l.arch == "amd64":
+		url += "darwin"
+	case l.os == "windows" && l.arch == "amd64":
+		url += ".exe"
+	case l.os == "linux" && l.arch == "amd64":
+	case l.os == "linux" && l.arch == "arm64":
+		url += "arm64"
+	default:
+		return "", &kpkgerr.UnsupportedRuntimeErr{Binary: l.Name()}
+	}
+	return url, nil
+}
+
+func MakeBinary(os, arch string) tool.Binary {
+	return k3supTool{
+		arch:              arch,
+		os:                os,
+		GithubReleaseTool: tool.MakeGithubReleaseTool("{{ .org }}", "{{ .repoName }}", 20),
+	}
+}
+`
+	// package names should be lowercase
+	pkgName := strings.ToLower(toolName)
+	// and shouldn't contain hyphens
+	pkgName = strings.ReplaceAll(pkgName, "-", "")
+	structName := toolName + "Tool"
+	tmpl, err := template.New("template code").Parse(templateCode)
+	if err != nil {
+		return err
+	}
+	err = tmpl.Execute(os.Stdout, map[string]string{
+		"pkgName":    pkgName,
+		"structName": structName,
+		"toolName":   toolName,
+		"org":        org,
+		"repoName":   repoName,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
