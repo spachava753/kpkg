8653035fdb5271e1169c0d56839af9fedd939752
Shashank Pachava <spachava753@gmail.com>
2021-03-20 02:14:13 -0400

    Finish adding code for target to generate code for new github release tool

--- magefile.go
+++ magefile.go
@@ -3,9 +3,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"github.com/spachava753/kpkg/pkg/util"
+	"io"
+	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"text/template"
 
@@ -101,7 +106,7 @@ func (l {{ .structName }}) MakeUrl(version string) (string, error) {
 }
 
 func MakeBinary(os, arch string) tool.Binary {
-	return k3supTool{
+	return {{ .structName }}{
 		arch:              arch,
 		os:                os,
 		GithubReleaseTool: tool.MakeGithubReleaseTool("{{ .org }}", "{{ .repoName }}", 20),
@@ -110,14 +115,41 @@ func MakeBinary(os, arch string) tool.Binary {
 `
 	// package names should be lowercase
 	pkgName := strings.ToLower(toolName)
-	// and shouldn't contain hyphens
+	// and shouldn't contain hyphens. This will also be the folder name
 	pkgName = strings.ReplaceAll(pkgName, "-", "")
-	structName := toolName + "Tool"
+
+	fmt.Printf("pkg name will be %s\n", pkgName)
+
+	// make sure that the folder doesn't exist
+	folderPath := filepath.Join("pkg", "tool", pkgName)
+	var folderExists bool
+	info, err := os.Stat(folderPath)
+	if err == nil {
+		if !info.IsDir() {
+			fmt.Errorf("found a file with the conflicting name %s, delete this file first", pkgName)
+		}
+		// since the dir already exists, make sure to check that we can create the file
+		var files []string
+		filepath.Walk(folderPath, func(path string, info fs.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+			files = append(files, path)
+			return nil
+		})
+		if files != nil && util.ContainsString(files, pkgName) {
+			fmt.Errorf("filepath %s already exists", filepath.Join(folderPath, pkgName))
+		}
+		folderExists = true
+	}
+
+	structName := ToLowerCamel(toolName) + "Tool"
 	tmpl, err := template.New("template code").Parse(templateCode)
 	if err != nil {
 		return err
 	}
-	err = tmpl.Execute(os.Stdout, map[string]string{
+	var bytes bytes.Buffer
+	err = tmpl.Execute(&bytes, map[string]string{
 		"pkgName":    pkgName,
 		"structName": structName,
 		"toolName":   toolName,
@@ -128,5 +160,81 @@ func MakeBinary(os, arch string) tool.Binary {
 		return err
 	}
 
-	return nil
+	// create a new folder if it doesn't exist
+	if !folderExists {
+		fmt.Printf("creating folder %s\n", folderPath)
+		if err := os.Mkdir(folderPath, 0775); err != nil {
+			return err
+		}
+	} else {
+		fmt.Printf("folder exists %s, skipping creation\n", folderPath)
+	}
+
+	// create the file
+	codePath := filepath.Join(folderPath, pkgName+".go")
+	fmt.Printf("creating file at location %s\n", codePath)
+	f, err := os.Create(codePath)
+	if err != nil {
+		return err
+	}
+
+	// and dump the code
+	fmt.Printf("dumping template code in %s\n", codePath)
+	_, err = io.Copy(f, &bytes)
+
+	return err
+}
+
+var uppercaseAcronym = map[string]string{
+	"ID": "id",
+}
+
+// Converts a string to CamelCase
+func toCamelInitCase(s string, initCase bool) string {
+	s = strings.TrimSpace(s)
+	if s == "" {
+		return s
+	}
+	if a, ok := uppercaseAcronym[s]; ok {
+		s = a
+	}
+
+	n := strings.Builder{}
+	n.Grow(len(s))
+	capNext := initCase
+	for i, v := range []byte(s) {
+		vIsCap := v >= 'A' && v <= 'Z'
+		vIsLow := v >= 'a' && v <= 'z'
+		if capNext {
+			if vIsLow {
+				v += 'A'
+				v -= 'a'
+			}
+		} else if i == 0 {
+			if vIsCap {
+				v += 'a'
+				v -= 'A'
+			}
+		}
+		if vIsCap || vIsLow {
+			n.WriteByte(v)
+			capNext = false
+		} else if vIsNum := v >= '0' && v <= '9'; vIsNum {
+			n.WriteByte(v)
+			capNext = true
+		} else {
+			capNext = v == '_' || v == ' ' || v == '-' || v == '.'
+		}
+	}
+	return n.String()
+}
+
+// ToCamel converts a string to CamelCase
+func ToCamel(s string) string {
+	return toCamelInitCase(s, true)
+}
+
+// ToLowerCamel converts a string to lowerCamelCase
+func ToLowerCamel(s string) string {
+	return toCamelInitCase(s, false)
 }
