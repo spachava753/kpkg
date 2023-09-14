40af7066539f37984cf7fa0d5aca0005355ed4f0
Shashank Pachava <spachava753@gmail.com>
2023-09-14 10:05:49 -0400

    Fix istio test

--- pkg/tool/istioctl/istioctl_test.go
+++ pkg/tool/istioctl/istioctl_test.go
@@ -103,10 +103,7 @@ func Test_istioctlTool_MakeUrl(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				l := istioctlTool{
-					arch: tt.fields.arch,
-					os:   tt.fields.os,
-				}
+				l := MakeBinary(tt.fields.os, tt.fields.arch)
 				got, err := l.MakeUrl(tt.args.version)
 				if (err != nil) != tt.wantErr {
 					t.Errorf(
